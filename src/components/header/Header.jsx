ab80e4db6d16471645eff252705302a622603786
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-07-12 19:34:44 -0500

    init

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+df
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Jeremy Fischer</h1>
+        <h5 className='text-light'>Software Engineer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </header>
+  )
+
+}
+
+export default Header
