d6a200914bb666ad68ce8d44e910d77e36a6c285
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-07-19 15:53:18 -0500

    Set up nav bar styling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,13 +5,14 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
-
+import {useState} from 'react'
 
 const Nav = () => {
+  const {activeNav, setActiveNav} = useState('#')
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#About"><AiOutlineUser/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#About" onClick={() => setActiveNav('#About')} className={activeNav === '#About' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#Experience"><BiBook/></a>
       <a href="#Services"><RiServiceLine/></a>
       <a href="#Contact"><BiMessageSquareDetail/></a>
