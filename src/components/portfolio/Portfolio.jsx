acd354c61beba54d2dceca21ac03d7d74fd26ff0
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-08-09 15:43:09 -0500

    Updates for portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
-import IMG2 from '../../assets/me.png'
+import IMG2 from '../../assets/vrSampleImage.png'
 import IMG3 from '../../assets/portfolio1.png'
 import IMG4 from '../../assets/portfolio1.png'
 import IMG5 from '../../assets/portfolio1.png'
@@ -17,33 +17,63 @@ const data = [
   },
   {
     id:2,
-    image: IMG1,
-    title: "Economic Research Paper w/ Professor Li Simonson",
-    github: "https://github.com/Jflyer45/CIS122-Project",
-    demo: "https://jflyer45.github.io/CIS122-Project/"
+    image: IMG2,
+    title: "Mankato VR Hospital Project",
+    github: "https://github.com/MNSU-CSET/Hospital_Simulator",
+    video: "https://www.youtube.com/watch?v=qQh-Kb8ev-A&ab_channel=NathanBursch"
   },
-  {
-    id:3,
-    image: IMG1,
-    title: "CIS 122 Data Structures Final Group Project",
-    github: "https://github.com/Jflyer45/CIS122-Project",
-    demo: "https://jflyer45.github.io/CIS122-Project/"
-  },
-  {
-    id:4,
-    image: IMG1,
-    title: "CIS 122 Data Structures Final Group Project",
-    github: "https://github.com/Jflyer45/CIS122-Project",
-    demo: "https://jflyer45.github.io/CIS122-Project/"
-  },
-  {
-    id:5,
-    image: IMG1,
-    title: "CIS 122 Data Structures Final Group Project",
-    github: "https://github.com/Jflyer45/CIS122-Project",
-    demo: "https://jflyer45.github.io/CIS122-Project/"
-  }
+  // {
+  //   id:3,
+  //   image: IMG1,
+  //   title: "CIS 122 Data Structures Final Group Project",
+  //   github: "https://github.com/Jflyer45/CIS122-Project",
+  //   demo: "https://jflyer45.github.io/CIS122-Project/"
+  // },
+  // {
+  //   id:4,
+  //   image: IMG1,
+  //   title: "CIS 122 Data Structures Final Group Project",
+  //   github: "https://github.com/Jflyer45/CIS122-Project",
+  //   demo: "https://jflyer45.github.io/CIS122-Project/"
+  // },
+  // {
+  //   id:5,
+  //   image: IMG1,
+  //   title: "CIS 122 Data Structures Final Group Project",
+  //   github: "https://github.com/Jflyer45/CIS122-Project",
+  //   demo: "https://jflyer45.github.io/CIS122-Project/"
+  // }
 ]
+
+const createGithubButton = (dataItem) => {
+  if (dataItem.github !== undefined){
+    return(
+      <a href={dataItem.github} className='btn' target='_blank'>Github</a>
+    )
+  }else{
+    return(null)
+  }
+}
+
+const createLiveDemoButton = (dataItem) => {
+  if (dataItem.demo !== undefined){
+    return(
+      <a href={dataItem.demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+    )
+  }else{
+    return(null)
+  }
+}
+
+const createVidoButton = (dataItem) => {
+  if (dataItem.video !== undefined){
+    return(
+      <a href={dataItem.video} className='btn btn-primary' target='_blank'>Demo Video</a>
+    )
+  }else{
+    return(null)
+  }
+}
 
 const createPortfolioItem = (dataItem) => {
     
@@ -55,8 +85,9 @@ const createPortfolioItem = (dataItem) => {
           <h3>{dataItem.title}</h3>
           {/* Call the action buttons */}
           <div className='portfolio__item-cta'>
-            <a href={dataItem.github} className='btn' target='_blank'>Github</a>
-            <a href={dataItem.demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+            {createGithubButton(dataItem)}
+            {createLiveDemoButton(dataItem)}
+            {createVidoButton(dataItem)}
           </div>
         </article>
     )
