83d46c5561ad71139eed5509093671c9a8d97811
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-08-09 14:27:58 -0500

    Fixed instagram not opening to new link

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -8,7 +8,7 @@ const HeaderSocial = () => {
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/jeremy-fischer/" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/Jflyer45" target="_blank"><FaGithub/></a>
-        <a href='https://www.instagram.com/jflyer45/'><FaInstagram/></a>
+        <a href='https://www.instagram.com/jflyer45/' target="_blank"><FaInstagram/></a>
     </div>
   )
 }
