4e6a61f82b5ab660291d48ce3c8888d6b9198a30
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-08-11 23:11:32 -0500

    Update Post schema

--- Backend/models/postModel.js
+++ Backend/models/postModel.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose')
 
+// The idea is to but content breaks into the text and insert the content at each break. Almost always http links
+// Ex. Hello in this video ?FLAG? I will be discussing this diagram ?FLAG?
 const postSchema = mongoose.Schema({
     title: {
         type: String,
         required: [true, "Title needed"]
-    }    
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    content: [{
+        type: String,
+        link: String
+    }]   
 }, {
     timestamps: true
 })
