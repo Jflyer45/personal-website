644acf8bebc8c87ecced8918f7356314ee4645aa
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-08-17 22:13:02 -0500

    Add main image, date, and more general styling for first real post

--- Frontend/src/Blog.jsx
+++ Frontend/src/Blog.jsx
@@ -5,9 +5,19 @@ import React, { useState, useEffect} from 'react';
 import './Blog.css'
 import BlogNav from './BlogNav';
 
+function dateFormater(rawDate) {
+  let year = rawDate.slice(0, 4)
+  let month = rawDate.slice(5, 7)
+  let day = rawDate.slice(8, 10)
+
+  return `${month}/${day}/${year}`
+}
+
 function Blog (){
     const [value, setValue] = useState("");
     const [title, setTitle] = useState("")
+    const [mainImage, setMainImage] = useState("")
+    const [date, setDate] = useState("")
     let {id} = useParams()
     
     console.log(id)
@@ -15,9 +25,11 @@ function Blog (){
     useEffect(() => {
       Axios.get(`http://localhost:8000/api/posts/${id}`)
       .then((response) => {
-        console.log(response)
+        console.log(response);
         setValue(response.data.text);
-        setTitle(response.data.title)
+        setTitle(response.data.title);
+        setMainImage(response.data.content[0].link);
+        setDate(dateFormater(response.data.createdAt))
       })
     }, []) // Second argurment, [], tells useEffect to rerender when that object changes
     // But since it is static, it never rerenders! Perfect, we only call api once
@@ -27,6 +39,8 @@ function Blog (){
         <BlogNav />
         <div className='content'>
             <div className='post'>
+                <img src={mainImage}></img>
+                <small>{date}</small>
                 <h2 className="title">{title}</h2>
                 <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} />
             </div>
